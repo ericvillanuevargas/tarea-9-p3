298e7d7e29ef53157b7cf4b0de9e79ca14657fa4
ericvillanuevargas <82411429+ericvillanuevargas@users.noreply.github.com>
2021-08-02 19:08:26 -0400

    Add files via upload

---
+++ Data/Consumo.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Tarea_9.Data
+{
+    public class Consumo
+    {
+        public dynamic Post(string url, string json, string autorizacion = null)
+        {
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+                if (autorizacion != null)
+                {
+                    request.AddHeader("Authorization", autorizacion);
+                }
+                IRestResponse response = client.Execute(request);
+                dynamic datos = JsonConvert.DeserializeObject(response.Content);
+                return datos;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public dynamic Get(string url)
+        {
+            try
+            {
+                HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                myWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0";
+                myWebRequest.Credentials = CredentialCache.DefaultCredentials;
+                myWebRequest.Proxy = null;
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myWebRequest.GetResponse();
+                Stream myStream = myHttpWebResponse.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myStream);
+
+                string Datos = HttpUtility.HtmlDecode(myStreamReader.ReadToEnd());
+                dynamic data = JsonConvert.DeserializeObject(Datos);
+                return data;
+            }
+            catch (Exception)
+            {
+                Console.Write("Lo sentimos, Ocurrio un error...");
+                return false;
+            }
+        }
+    }
+}
---
+++ Data/ExampleModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea_9.Data
+{
+    public class ExampleModel
+    {
+        [Required(ErrorMessage = "El campo Cedula es requerido")]
+        [StringLength(11, ErrorMessage = "El Cedula es muy largo")]
+        public string Cedula { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Apellido es requerido")]
+        public string Apellido{ get; set; }
+        [Required(ErrorMessage = "El campo Telefono es requerido")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Correo es requerido")]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "El campo Fecha de nacimiento es requerido")]
+        public string Fecha_Nacimiento { get; set; }
+        [Required(ErrorMessage = "El campo Tipo de sangre es requerido")]
+        public string Tipo_Sangre { get; set; }
+        [Required(ErrorMessage = "El campo Provincia es requerido")]
+        public string Provincia { get; set; }
+        [Required(ErrorMessage = "El campo Direccion es requerido")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "El campo Longitud es requerido")]
+        public string Longitud { get; set; }
+        [Required(ErrorMessage = "El campo Latitud es requerido")]
+        public string Latitud { get; set; }
+        [Required(ErrorMessage = "El campo Vacunacion es requerido")]
+        public string Vacunacion { get; set; }
+        [Required(ErrorMessage = "El campo Justificacion es requerido")]
+        public string Justificacion { get; set; }
+        [Required(ErrorMessage = "El campo Municipio es requerido")]
+        public string Municipio { get; set; }
+        [Required(ErrorMessage = "El campo Distrito Municipal es requerido")]
+        public string Distrito_Municipal { get; set; }
+        [Required(ErrorMessage = "El campo Nombre de la calle es requerido")]
+        public string Nombre_Calle{ get; set; }
+        [Required(ErrorMessage = "El campo Numero de la calle es requerido")]
+        public string No_Calle { get; set; }
+        [Required(ErrorMessage = "El campo numero de la casa es requerido")]
+        public string No_Casa { get; set; }
+
+    }
+}
